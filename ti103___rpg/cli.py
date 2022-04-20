c8e4eb8365e41a6e38db0bf074313d9cb5ca8934
rudy <erwanmboda@gmail.com>
2022-04-19 22:49:38 -0500

    ti103___rpg

--- ti103___rpg/cli.py
+++ ti103___rpg/cli.py
@@ -1,20 +1,7 @@
-"""Console script for ti103___rpg."""
-import argparse
-import sys
 
-
-def main():
-    """Console script for ti103___rpg."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
-    args = parser.parse_args()
-
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "ti103___rpg.cli.main")
-    return 0
 
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
 #rajoutant du balbla a ce code 
+#pourqoi pas mettre encore 
